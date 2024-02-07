2937cbf17e36fc4a78a8e78d4d4b762e82bdfbbb
shwetap7 <shweta_patil7@persistent.com>
2024-02-07 15:06:30 +0530

    dummy entry added in payrolltabledata

--- src/app/models/payroll-table-details.ts
+++ src/app/models/payroll-table-details.ts
@@ -20,4 +20,5 @@ export const ELEMENT_DATA: PayrollData[] = [
   {country: "UK", payroll: 'Weekly Payroll Production staff', date: "29th March", stage: 'Collate', critical: 1, overdue: 1, due:1, querie: 1},
   {country: "USA", payroll: 'Monthly Payroll Production staff', date: "29th March", stage: 'Collate', critical: 1, overdue: 1, due:1, querie: 1},
   {country: "India", payroll: 'Fortnightly Payroll Production staff', date: "29th March", stage: 'Collate', critical: 1, overdue: 1, due:1, querie: 1},
+  {country: "USA", payroll: 'Weekly Payroll Production staff', date: "29th March", stage: 'Collate', critical: 1, overdue: 1, due:1, querie: 1},
 ];
