d3f99b60abb3d7ab89543bdca1efc8afc289f921
sima-15 <sima_kumari@persistent.com>
2024-02-15 18:56:46 +0530

    styling pixed on landing page

--- src/app/shared/filter/filter.component.ts
+++ src/app/shared/filter/filter.component.ts
@@ -8,6 +8,27 @@ import { FormControl } from '@angular/forms';
 })
 export class FilterComponent {
   selectedOption: string = 'Most Relevant';
-  options = ["Australia", "Canada", "China", "France", "Germany"];
+  // options = ["Australia", "Canada", "China", "France", "Germany"];
   selected = [];
+  options = [
+    {
+      name: "Australia",
+      icon: '../../../assets/image/GB.png'
+    },
+    {
+      name: "Canada",
+      icon: '../../../assets/image/GB.png'
+    },
+    {
+      name: "China",
+      icon: '../../../assets/image/GB.png'
+    },
+    {
+      name: "France",
+      icon: '../../../assets/image/GB.png'
+    },
+    {
+      name: "Germany",
+      icon: '../../../assets/image/GB.png'
+    }];
 }
